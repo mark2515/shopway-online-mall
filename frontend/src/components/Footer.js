823fbdd9830e42f01278953734890e567d904029
Dragon4396 <hekunlong2515@gmail.com>
2024-05-25 21:29:04 -0700

    create header and footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+            <Row>
+                <Col className="text-center py-3">
+                    CopyRight &copy; ShopWay
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
