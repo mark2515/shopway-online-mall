a40e618d53ce275a75622ff5058b65ed5783db4f
Dragon4396 <hekunlong2515@gmail.com>
2024-06-01 23:00:40 -0700

    install color.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -23,4 +24,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`The server is running on port ${PORT} under ${process.env.NODE_ENV} mode`))
+app.listen(PORT, console.log(`The server is running on port ${PORT} under ${process.env.NODE_ENV} mode`.yellow.bold))
