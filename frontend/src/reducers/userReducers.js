5f6233ed4895f7ee537981b40b3f63591152a069
Dragon4396 <hekunlong2515@gmail.com>
2024-06-24 17:47:11 -0700

    create user details screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -5,7 +5,10 @@ import {
   USER_LOGOUT,
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS
+  USER_REGISTER_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS
 } from '../constants/userConstants'
   
 //User login reducer
@@ -32,6 +35,20 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: action.payload }
     case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+//User details reducer
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { loading: true, ...state }
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload }
+    case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
