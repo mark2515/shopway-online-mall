8788e591c7ced8a5cb8a8c9926ffd00ab21acc27
Dragon4396 <hekunlong2515@gmail.com>
2024-06-30 23:21:42 -0700

    create orderDetails reducer&action

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -5,32 +5,61 @@ import {
 } from '../constants/orderConstants'
 import axios from 'axios'
 
-  //Create order action
-  export const createOrder = (order) => async (dispatch, getState) => {
-    try {
-      dispatch({ type: ORDER_CREATE_REQUEST })
-  
-      //Get user information after successful login
-      const {
-        userLogin: { userInfo },
-      } = getState()
-  
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${userInfo.token}`,
-        },
-      }
-  
-      const { data } = await axios.post(`/api/orders`, order, config)
-      dispatch({ type: ORDER_CREATE_SUCCESS, payload: data })
-    } catch (error) {
-      dispatch({
-        type: ORDER_CREATE_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      })
+//Create order action
+export const createOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_CREATE_REQUEST })
+
+    //Get user information after successful login
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
     }
+
+    const { data } = await axios.post(`/api/orders`, order, config)
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
   }
+}
+
+//Get order action
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST })
+
+    //Get user information after successful login
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/orders/${id}`, config)
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
     ORDER_CREATE_FAIL,
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
+    ORDER_DETAILS_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS
 } from '../constants/orderConstants'
   
 //Create order reducer
@@ -16,4 +19,21 @@ export const orderCreateReducer = (state = {}, action) => {
       default:
         return state
     }
+}
+
+//Get order reducer
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddress: {} },
+  action
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return { ...state, loading: true }
+    case ORDER_DETAILS_SUCCESS:
+      return { loading: false, order: action.payload }
+    case ORDER_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
 }
