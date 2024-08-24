50613875ca138c6e24a26e459a75c14b1a402040
Dragon4396 <hekunlong2515@gmail.com>
2024-08-23 18:32:20 -0700

    userList safety access

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -14,7 +14,8 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
-  USER_LIST_FAIL
+  USER_LIST_FAIL,
+  USER_LIST_RESET
 } from '../constants/userConstants'
 import axios from 'axios'
 
@@ -52,6 +53,7 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_LIST_RESET })
 }
 
 //User register action
