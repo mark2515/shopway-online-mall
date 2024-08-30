b894197f086bccbe95a9a8f8cb3b3b4375e8d466
Dragon4396 <hekunlong2515@gmail.com>
2024-08-29 23:24:48 -0700

    prepare deployment

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/', (req, res) => {
-    res.send('The server is running')
-})
-
 //Get PayPal's clientID
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
@@ -42,8 +38,18 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.use(notFound)
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve)(__dirname, 'frontend', 'build', 'index.html')
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('The server is running')
+    })
+}
 
+app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
