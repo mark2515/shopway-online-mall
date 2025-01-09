53639385c01abb3a307bc1b9ba7b5433cc6334d6
mark2515 <hekunlong2515@gmail.com>
2025-01-08 18:26:50 -0800

    change product image size

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -80,7 +80,7 @@ const ProductScreen = ({history, match}) => {
           <Meta title={product.name} />      
           <Row>
             <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
+              <Image src={product.image} alt={product.name} fluid style={{ width: '800px', height: '400px' }} />
             </Col>
             <Col md={3}>
               <ListGroup variant='flush'>
